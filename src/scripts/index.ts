effb790fcdb1800551cbc9478b8565512f6028d0
Lucas Tanaka <160812489+Chico7854@users.noreply.github.com>
2024-12-27 23:27:18 -0300

    Finish Project

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,26 +4,18 @@ import "../styles/styles.css";
 import { Compatibility } from "./compatibility";
 
 class App {
+    static name1Element = document.querySelector("input[name='your-name']") as HTMLInputElement;
+    static name2Element = document.querySelector("input[name='their-name']") as HTMLInputElement;
+
     static init() {
-        const name1Element = document.querySelector("input[name='your-name']") as HTMLInputElement;
-        const name2Element = document.querySelector("input[name='their-name']") as HTMLInputElement;
         const checkCompatibilityButton = document.querySelector("button") as HTMLButtonElement;
 
-        checkCompatibilityButton.addEventListener("click", () => {
-            const name1 = name1Element.value;
-            const name2 = name2Element.value;
-            name1Element.value = "";
-            name2Element.value = "";
-
-            const compatibility = new Compatibility(name1, name2);
-
-            if (compatibility.isPerfectCouple) {
-                this.updateResult(100000000000000000000000);
-            } else {
-                const result = compatibility.randomizeCompatibility();
-                this.updateResult(result);
+        checkCompatibilityButton.addEventListener("click", App.calculateCompatibilityHandler);
+        document.addEventListener("keyup", event => {
+            if (event.key === "Enter") {
+                App.calculateCompatibilityHandler();
             }
-        });
+        })
     }
 
     static updateResult(result: number): void {
@@ -40,6 +32,39 @@ class App {
                 clearInterval(animation);
             }
         }, individualNumberAnimationTime);
+    }
+
+    static checkInput(): boolean {
+        if (App.name1Element.value === "" || App.name2Element.value === "") {
+            alert("Preencha os nomes por favor.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    static filterName(name: string): string {
+        let firstName = name.split(" ")[0];
+        firstName = firstName.toLowerCase();
+        return firstName;
+    }
+
+    static calculateCompatibilityHandler() {
+        if (!App.checkInput()) return;
+
+        const name1 = App.filterName(App.name1Element.value);
+        const name2 = App.filterName(App.name2Element.value);
+        App.name1Element.value = "";
+        App.name2Element.value = "";   
+
+        const compatibility = new Compatibility(name1, name2);
+
+        if (compatibility.isPerfectCouple) {
+            App.updateResult(100000000000000000000000);
+        } else {
+            const result = compatibility.randomizeCompatibility();
+            App.updateResult(result);
+        }
     }
 }
 
